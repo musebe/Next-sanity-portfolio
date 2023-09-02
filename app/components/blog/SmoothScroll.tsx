b1732bb2f20989e95c15e11bf4994130cdb54fa7
musebe <emusebe9@gmail.com>
2023-09-02 20:18:37 +0300

    framer motion modification

--- app/components/blog/SmoothScroll.tsx
+++ app/components/blog/SmoothScroll.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 interface SmoothScrollProps {
   children: React.ReactNode;
@@ -9,6 +9,26 @@ const SmoothScroll: React.FC<SmoothScrollProps> = ({
   children,
   speed = 0.1,
 }) => {
+  useEffect(() => {
+    const scrollContainer = document.getElementById('smooth-scroll-container');
+
+    const handleScroll = (e: { deltaY: number; }) => {
+      if (scrollContainer) {
+        if (e.deltaY > 0) {
+          scrollContainer.scrollTop += 20;
+        } else if (e.deltaY < 0) {
+          scrollContainer.scrollTop -= 20;
+        }
+      }
+    };
+
+    window.addEventListener('wheel', handleScroll);
+
+    return () => {
+      window.removeEventListener('wheel', handleScroll);
+    };
+  }, []);
+
   return (
     <div
       id='smooth-scroll-container'
